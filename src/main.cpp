7777a0b434f6d92751185a1d9bfe66ddaf8d6e4f
Florian Sylvain <sylvainflorian@proton.me>
2026-03-19 19:11:27 +0100

    fix: refactor SDL and Vulkan init

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,22 @@
 
 const auto APPLICATION_NAME = "Vulkan M4 Setup";
 
-VkInstance* initVulkan() {
+SDL_Window *initSDL3() {
+    if (SDL_Init(SDL_INIT_VIDEO) == false) {
+        SDL_Log("Erreur SDL_Init : %s", SDL_GetError());
+        throw std::runtime_error("Failed to initialize SDL");
+    }
+
+    SDL_Window* window = SDL_CreateWindow(APPLICATION_NAME, 800, 600, SDL_WINDOW_VULKAN);
+    if (!window) {
+        std::cerr << "Erreur création fenêtre : " << SDL_GetError() << std::endl;
+        throw std::runtime_error("Failed to create window");
+    }
+
+    return window;
+}
+
+VkInstance initVulkan() {
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pApplicationName = APPLICATION_NAME;
@@ -22,30 +37,36 @@ VkInstance* initVulkan() {
     uint32_t extensionCount = 0;
     const char* const* extensions = SDL_Vulkan_GetInstanceExtensions(&extensionCount);
 
-    createInfo.enabledExtensionCount = extensionCount;
-    createInfo.ppEnabledExtensionNames = extensions;
+    std::vector<const char*> requiredExtensions;
+    for (uint32_t i = 0; i < extensionCount; i++) {
+        requiredExtensions.emplace_back(extensions[i]);
+    }
+
+    requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+    createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+
+    createInfo.enabledExtensionCount = (uint32_t) requiredExtensions.size();
+    createInfo.ppEnabledExtensionNames = requiredExtensions.data();
 
     createInfo.enabledLayerCount = 0;
 
-    VkInstance instance = VK_NULL_HANDLE;
-    auto result = vkCreateInstance(&createInfo, nullptr, &instance);
+    VkInstance instance;
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create instance!");
+    }
 
-    return &instance;
+    return instance;
+}
+
+void cleanup(VkInstance* instance, SDL_Window* window) {
+    vkDestroyInstance(*instance, nullptr);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
 
 int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) == false) {
-        SDL_Log("Erreur SDL_Init : %s", SDL_GetError());
-        return 1;
-    }
-
-    SDL_Window* window = SDL_CreateWindow(APPLICATION_NAME, 800, 600, SDL_WINDOW_VULKAN);
-    if (!window) {
-        std::cerr << "Erreur création fenêtre : " << SDL_GetError() << std::endl;
-        return 1;
-    }
-
-    initVulkan();
+    SDL_Window* window = initSDL3();
+    VkInstance instance = initVulkan();
 
     bool running = true;
     while (running) {
@@ -57,8 +78,7 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    cleanup(&instance, window);
 
     return 0;
 }
