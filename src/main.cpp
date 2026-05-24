84eb5bf201e1e1a7d578b4ce62429c35dfa80d8a
Florian Sylvain <sylvainflorian@proton.me>
2026-05-24 17:45:48 +0200

    clean: removed vulkan stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,3 @@
-#include <core/Device.hpp>
-#include <core/Instance.hpp>
-#include <core/Pipeline.hpp>
-#include <core/Renderer.hpp>
-#include <core/SwapChain.hpp>
 #include <core/Window.hpp>
 
 #ifdef NDEBUG
@@ -13,20 +8,10 @@ const bool enableValidationLayers = true;
 
 int main() {
     Window window("volkanite", 800, 600);
-    Instance instance(window, enableValidationLayers);
-    Device device(instance);
-    SwapChain swapChain(window, instance, device);
-    Pipeline pipeline(device, swapChain);
-    Renderer renderer(device, swapChain, pipeline);
-
-    window.setChangeCallback([&](int w, int h) { renderer.setFramebufferResized(); });
 
     while (window.isRunning()) {
         window.pollEvents();
-        renderer.render();
     };
-
-    device.getLogicalDevice().waitIdle();
 
     return 0;
 }
